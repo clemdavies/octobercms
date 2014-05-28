e07d5e8ca9f80c3751e50e92ec6ecff8dfc27123
Clem Davies <clem.wdavies@gmail.com>
2014-05-28 18:13:16 +0800

    fixed permissions

---
+++ app/storage/twig/80/33/60595f39cc605a9ec756351fa667d06f30beb5a5b5ce57f2670ae3b5bd53.php
@@ -0,0 +1,35 @@
+<?php
+
+/* /home/clem/Dropbox/october/themes/demo/partials/footer.htm */
+class __TwigTemplate_803360595f39cc605a9ec756351fa667d06f30beb5a5b5ce57f2670ae3b5bd53 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<div id=\"footer\">
+    <div class=\"container\">
+        <p class=\"muted credit\">&copy; 2013 Alexey Bobkov &amp; Samuel Georges.</p>
+    </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "/home/clem/Dropbox/october/themes/demo/partials/footer.htm";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  129 => 59,  125 => 58,  121 => 57,  118 => 56,  116 => 53,  111 => 50,  107 => 49,  101 => 45,  99 => 44,  82 => 34,  74 => 33,  66 => 32,  59 => 28,  40 => 11,  37 => 9,  34 => 8,  30 => 7,  24 => 4,  19 => 1,  174 => 97,  168 => 93,  160 => 120,  134 => 97,  127 => 93,  105 => 74,  87 => 59,  68 => 43,  27 => 4,  25 => 3,  21 => 1,);
+    }
+}
